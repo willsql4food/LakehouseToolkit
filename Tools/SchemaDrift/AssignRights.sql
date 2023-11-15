f9ef4facffcb3c7d0c82bc293652c4199a2f5179
Carter Burleigh <Carter.Burleigh@flooranddecor.com>
2023-11-15 16:48:50 -0500

    Added conditional user creation logic

--- Tools/SchemaDrift/AssignRights.sql
+++ Tools/SchemaDrift/AssignRights.sql
@@ -1,7 +1,10 @@
 /* ====================================================================================================================
     Grant rights to the ADF account to execute the uspConformTable stored procedure, read and write data in all tables
 ==================================================================================================================== */
-grant execute on dbo.uspConformTable to [df-sbx-ab092898]
+if not exists (select * from sys.sysusers where name = 'df-sbx-ab092898' )
+    create user [df-sbx-ab092898] from external provider
+go
+grant execute to [df-sbx-ab092898]
 alter role db_datareader add member [df-sbx-ab092898]
 alter role db_datawriter add member [df-sbx-ab092898]
 alter role db_ddladmin add member [df-sbx-ab092898]
