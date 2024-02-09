02e3f591783524159c6b8b3cf3e3bf18ababf488
Carter Burleigh <Carter.Burleigh@flooranddecor.com>
2024-02-09 16:41:38 -0500

    Tweak to handle sub-batch counts

--- Tools/GoogleAnalytics/Data Load Status.sql
+++ Tools/GoogleAnalytics/Data Load Status.sql
@@ -1,3 +1,4 @@
+/* Simple summary of what has been loaded and any marked to load (assume in progress) */
 select      case when LoadedDateUTC is not null then 'Loaded' else 'Waiting' end msg,
             count(*) BatchCount,
             format(sum(RowCountSource), 'N0') RowsToLoad, 
@@ -6,10 +7,12 @@ select      case when LoadedDateUTC is not null then 'Loaded' else 'Waiting' end
 from        dbo.gbqWatermark
 group by    case when LoadedDateUTC is not null then 'Loaded' else 'Waiting' end
 
-
-select      case when RowCountSource <> RowCountDest then '***' else '' end Mismatch,
-            LoadedDateUTC, TableName, BatchId, RowCountSource, RowCountDest, 
+/* Rows Loaded and variance - must calculate at batch level, accounting for potential sub-batches */
+select      case when s.RowCountSource <> s.RowCountDest then '***' else '' end Mismatch,
+            LoadedDateUTC, a.TableName, a.BatchId, a.SubBatchId, RowCountSource, RowCountDest, 
             RowCountSource - RowCountDest Variance
-from        dbo.gbqWatermark
+from        dbo.gbqWatermark a
+left join  (select  TableName, BatchId, RowCountSource = sum(RowCountSource), RowCountDest = sum(RowCountDest), NumSubBatches = count(*) 
+            from    dbo.gbqWatermark group by TableName, BatchId) s on s.TableName = a.TableName and s.BatchId = a.BatchId
 where       LoadedDateUTC is not null
-order by    Mismatch desc, right(TableName, 8) desc, TableName, BatchId
+order by    Mismatch desc, right(TableName, 8) desc, TableName, BatchId, SubBatchId
