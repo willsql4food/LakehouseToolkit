965f5b734b3e14bdab35395e845f516671a85df7
Carter Burleigh <Carter.Burleigh@flooranddecor.com>
2024-02-12 14:27:37 -0500

    Tweak alias pattern

--- Tools/GoogleAnalytics/Data Load Status.sql
+++ Tools/GoogleAnalytics/Data Load Status.sql
@@ -8,11 +8,13 @@ from        dbo.gbqWatermark
 group by    case when LoadedDateUTC is not null then 'Loaded' else 'Waiting' end
 
 /* Rows Loaded and variance - must calculate at batch level, accounting for potential sub-batches */
-select      case when s.RowCountSource <> s.RowCountDest then '***' else '' end Mismatch,
-            LoadedDateUTC, a.TableName, a.BatchId, a.SubBatchId, RowCountSource, RowCountDest, 
-            RowCountSource - RowCountDest Variance
+select      Mismatch = case when s.RowCountSource <> s.RowCountDest then '***' else '' end,
+            a.LoadedDateUTC, a.TableName, a.BatchId, a.SubBatchId, 
+            s.RowCountSource, s.RowCountDest, 
+            Variance = s.RowCountSource - s.RowCountDest,
+            BatchRowCountDest = a.RowCountDest
 from        dbo.gbqWatermark a
 left join  (select  TableName, BatchId, RowCountSource = sum(RowCountSource), RowCountDest = sum(RowCountDest), NumSubBatches = count(*) 
             from    dbo.gbqWatermark group by TableName, BatchId) s on s.TableName = a.TableName and s.BatchId = a.BatchId
 where       LoadedDateUTC is not null
-order by    Mismatch desc, right(TableName, 8) desc, TableName, BatchId, SubBatchId
+order by    Mismatch desc, right(a.TableName, 8) desc, a.TableName, a.BatchId, a.SubBatchId
