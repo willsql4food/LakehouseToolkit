aa2f1a991ca145725b970753e172eb37fda4ac73
Carter Burleigh <Carter.Burleigh@flooranddecor.com>
2024-02-27 11:16:21 -0500

    Better code

--- Tools/GoogleAnalytics/Watermark Change for Reload.sql
+++ Tools/GoogleAnalytics/Watermark Change for Reload.sql
@@ -1,37 +1,37 @@
-/*
-select * from stage.gbqObject where TableName like '%20240101'
-select * from stage.gbqObjectDetail where TableName like '%20240101'
+/* ====================================================================================================================
+    Exprloration
+==================================================================================================================== */
+/* How many rows are there, and how many are not yet loaded? */
+-- select      count(*) numRows, sum(case when wm.LoadedDateUTC is null then 1 else 0 end) numToLoad 
+-- from        dbo.gbqWatermark wm
 
-select * from dbo.gbqObjectToLoad
-select * from dbo.gbqObjectToLoadCmd
+/* Sample a dozen rows */
+-- select      top 12 *
+-- from        dbo.gbqWatermark wm
+-- where       wm.LoadedDateUTC is null
 
-select * from dbo.gbqWatermark order by TableCatalog, TableSchema, TableName, BatchId
+/* Find batches for rows not actually loaded */
+select      right(TableName, 8) table_suffix
+        ,   count(*) num_rows
+        ,   min(LoadedDateUTC) earliest_load
+        ,   max(LoadedDateUTC) latest_load
+from        dbo.gbqWatermark wm
+where       wm.RowCountDest is null
+group by    right(TableName, 8)
+order by    right(TableName, 8)
 
-/* Reload a few files - delete or move the original files from the storage account also... 
----------------------------------------------------------------------------------------------
-update  dbo.gbqWatermark 
-set     LoadedDateUTC = null, RowCountDest = null, DestinationUri = null
-where   Id between 292 and 299
-*/
 
-/* Any batches with multiple watermark records? */
-select      TableCatalog, TableSchema, TableName, BatchId
-from        dbo.gbqWatermark
-group by    TableCatalog, TableSchema, TableName, BatchId
-having      count(*) > 1
+/* ====================================================================================================================
+    Action
+==================================================================================================================== */
+/* Mark everything as loaded */
+-- update dbo.gbqWatermark set LoadedDateUTC = '1971-01-01' where LoadedDateUTC is null
 
-select top 20 * from dbo.gbqWatermark order by Id desc
+/* Turn on a single day */
+update      dbo.gbqWatermark 
+set         LoadedDateUTC = null
+        ,   RowCountDest = null
+where       TableName like '%users%20240225'
 
-select top 20 * from dbo.adfPipelineExecution order by StartTimeUtc desc
-
--- update  dbo.adfPipelineExecution set StatusMessage = 'Testing' where RunId = '1aa10904-0789-4dce-9460-11aa17bd3900'
-*/
-
-select      TableName, max(BatchId) highestBatchId, count(BatchId) countBatchId, format(sum(RowCountSource), 'N0') numRows
-from        dbo.gbqWatermark
-group by    TableName
-order by    right(TableName, 8) desc, TableName
-
-select      top 10 *
-from        stage.gbqObjectDetail
-order by    RowcountSource desc
+select		*
+from		dbo.gbqObjectToLoad
