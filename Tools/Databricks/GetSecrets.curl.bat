0c49a18bee33daca13ed792b36bc10872251f444
Carter Burleigh <144727561+AB092898@users.noreply.github.com>
2024-10-02 11:27:30 -0400

    Delete Tools/Databricks/GetSecrets.curl.bat

--- Tools/Databricks/GetSecrets.curl.bat
+++
@@ -1,3 +0,0 @@
-echo Getting list of secret scopes
-curl --request GET "https://adb-7286372339506263.3.azuredatabricks.net/api/2.0/secrets/scopes/list" \
-    --header "Authorization: Bearer dapice801c23fe40c263df24097111797354-3"
